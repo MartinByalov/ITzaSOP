ff094905b4e832efa4f8d5dd612553d50940a8a7
MartinByalov <35262068+MartinByalov@users.noreply.github.com>
2022-02-08 17:25:31 +0200

    Add files via upload

---
+++ js/initpointer.js
@@ -0,0 +1,5 @@
+init_pointer({
+                pointerColor: "black", // Css color
+                ringSize: 25, // Pixels
+                ringClickSize: 10 // Pixels when clicking 
+            })
